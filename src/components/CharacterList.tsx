309b415262af6791d3721e3bd1cfe77ff590b5a1
Petr Bui <buihonganh1@gmail.com>
2020-11-11 00:13:00 +0100

    added function sort by Alphabet

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -4,12 +4,12 @@ import "../App.scss";
 
 const CharacterList = () => {
   const [data, setData] = useState<any[]>([]);
-
+  const [isSortedByAlphabet, setIsSortedByAlphabet] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
 
-  const fetchData = async () => {
+  const fetchData = () => {
     axios
       .get("https://finalspaceapi.com/api/v0/character")
       .then((res) => {
@@ -20,25 +20,92 @@ const CharacterList = () => {
       });
   };
 
-  return (
-    <div className='CharacterList'>
-      <h1>Final Space Characters</h1>
-      <div className='CharacterList__table'>
-        {data.map((character) => (
-          <div className='CharacterList__table__item' key={character.id}>
-            <div>
-              <img
-                className='CharacterList__table__item__img'
-                src={character.img_url}
-                alt={character.alias}
-              />
+  const renderDefaultList = (data: any) => {
+    return data.map((item: any) => (
+      <div className='characters__list__item' key={item.id}>
+        <div>
+          <img
+            className='characters__list__item-img'
+            src={item.img_url}
+            alt={item.alias}
+          />
 
-              <p className='CharacterList__table__item__name'>
-                {character.name}
-              </p>
-            </div>
+          <p className='characters__list__item-name'>{item.name}</p>
+        </div>
+      </div>
+    ));
+  };
+
+  const renderByAlphabet = (list: Array<string>) => {
+    const alphabet = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+
+    return alphabet.map((char) => {
+      const filteredData = list.filter(
+        (item: any) => item.name && item.name.toLowerCase().startsWith(char)
+      );
+
+      return (
+        filteredData.length > 0 && (
+          <div className='characters__list'>
+            <p className='characters__list-alphabet'>{char}</p>
+            {filteredData.map((item: any) => (
+              <div className='characters__list__item' key={item.id}>
+                <div>
+                  <img
+                    className='characters__list__item-img'
+                    src={item.img_url}
+                    alt={item.alias}
+                  />
+
+                  <p className='characters__list__item-name'>{item.name}</p>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        )
+      );
+    });
+  };
+
+  return (
+    <div className='characters'>
+      <h1>Final Space Characters</h1>
+      <button
+        className='characters-btn'
+        onClick={() => setIsSortedByAlphabet(!isSortedByAlphabet)}
+      >
+        {!isSortedByAlphabet ? "Sort by alphabet" : "Sort by id"}
+      </button>
+
+      <div className='characters__table'>
+        {isSortedByAlphabet ? renderByAlphabet(data) : renderDefaultList(data)}
       </div>
     </div>
   );
